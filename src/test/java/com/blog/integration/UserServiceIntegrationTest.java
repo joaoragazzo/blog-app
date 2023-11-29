1bfa8c14c0ef33b85efa9847344bc6290af34e85
João Ragazzo <joao_ragazzo@hotmail.com>
2023-11-29 02:45:08 -0300

    feat: implementing user service integration test

--- src/test/java/com/blog/integration/UserServiceIntegrationTest.java
+++ src/test/java/com/blog/integration/UserServiceIntegrationTest.java
@@ -1,6 +1,9 @@
 package com.blog.integration;
 
+import com.blog.entity.User;
+import com.blog.exception.UserNotFoundException;
 import com.blog.service.UserService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,19 +21,30 @@ public class UserServiceIntegrationTest
     @Test
     @DisplayName("findById > When the id is null > Throw an exception")
     void findByIdWhenTheIdIsNullThrowAnException() {
-        fail(" Not implemented");
+        Assertions.assertThrows(IllegalAccessError.class,
+                () -> service.findById(null));
     }
 
     @Test
     @DisplayName("findById > When the id is not null > When a user is found > Return the user")
     void findByIdWhenTheIdIsNotNullWhenAUserIsFoundReturnTheUser() {
-        fail(" Not implemented");
+        User user = service.findById(6);
+
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(6, user.getId()),
+                () -> Assertions.assertEquals("Mrs. Dennis Schulist", user.getName()),
+                () -> Assertions.assertEquals("Leopoldo_Corkery", user.getUsername()),
+                () -> Assertions.assertEquals("Karley_Dach@jasper.info", user.getEmail()),
+                () -> Assertions.assertEquals("1-477-935-8478 x6430", user.getPhone()),
+                () -> Assertions.assertEquals("ola.org", user.getWebsite())
+        );
     }
 
     @Test
     @DisplayName("findById > When the id is not null > When no user is found > Throw an exception")
     void findByIdWhenTheIdIsNotNullWhenNoUserIsFoundThrowAnException() {
-        org.junit.jupiter.api.Assertions.fail(" Not implemented");
+       Assertions.assertThrows(UserNotFoundException.class,
+               () -> service.findById(11));
     }
 
 }
