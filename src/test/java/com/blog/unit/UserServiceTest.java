1545f560a4fb7524d09de25e84837168d75580b3
João Ragazzo <joao_ragazzo@hotmail.com>
2023-11-29 02:49:35 -0300

    test: adding basic concept to user service test

---
+++ src/test/java/com/blog/unit/UserServiceTest.java
@@ -0,0 +1,28 @@
+package com.blog.unit;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class UserServiceTest
+{
+    @Test
+    @DisplayName("findById > When the id is null > Throw an exception")
+    void findByIdWhenTheIdIsNullThrowAnException() {
+        fail(" Not implemented");
+    }
+
+    @Test
+    @DisplayName("findById > When the id is not null > When the user is found > Return user")
+    void findByIdWhenTheIdIsNotNullWhenTheUserIsFoundReturnUser() {
+        fail(" Not implemented");
+    }
+
+    @Test
+    @DisplayName("findById > When the id is not null > When the user is found > Throw an exception")
+    void findByIdWhenTheIdIsNotNullWhenTheUserIsFoundThrowAnException() {
+        org.junit.jupiter.api.Assertions.fail(" Not implemented");
+    }
+
+}
